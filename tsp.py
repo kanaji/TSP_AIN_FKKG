9f21f4970d3f1885dca30def6b35e40c342aee3e
FLap\Kanaji <pilifok2@gmail.com>
2022-01-08 22:20:28 +0100

    Base GUI

---
+++ tsp.py
@@ -0,0 +1,14 @@
+from PyQt5 import QtWidgets, uic
+import sys
+
+
+class Ui(QtWidgets.QDialog):
+    def __init__(self):
+        super(Ui, self).__init__()
+        uic.loadUi('tsp.ui', self)
+        self.show()
+
+
+app = QtWidgets.QApplication(sys.argv)
+window = Ui()
+app.exec_()
