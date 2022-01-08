cfa97d9bfa9a4022d699d7f1665ba4ce75846d55
Karol Gor <kameha1805@gmail.com>
2022-01-08 22:33:56 +0100

    Base Genetic Algorithm Implementation

--- tsp.py
+++ tsp.py
@@ -1,5 +1,6 @@
 from PyQt5 import QtWidgets, uic
 import sys
+import genetic_algorithm
 
 
 class Ui(QtWidgets.QDialog):
@@ -12,3 +13,11 @@ class Ui(QtWidgets.QDialog):
 app = QtWidgets.QApplication(sys.argv)
 window = Ui()
 app.exec_()
+
+cityList = []
+
+for i in range(0,25):
+    cityList.append(City(x=int(random.random() * 200), y=int(random.random() * 200)))
+
+geneticAlgorithm(population=cityList, popSize=100, eliteSize=20, mutationRate=0.01, generations=500)
+geneticAlgorithmPlot(population=cityList, popSize=100, eliteSize=20, mutationRate=0.01, generations=500)
